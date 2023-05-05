476c0788f7d50c11bfd4551352a0b6d0db62ccbd
Hamza Rizvi <hamzarizvii@hotmail.com>
2023-05-05 16:30:23 +0500

    REDESIGNED

--- lib/viewmodel/global/navbar/vm_navbar.dart
+++
@@ -1,8 +0,0 @@
-
-import 'package:flutter/cupertino.dart';
-import 'package:portfolio/viewmodel/global/navbar/bloc_navbar.dart';
-
-class ViewModelNavBar {
-  EnumsNavBar selectedScreen = EnumsNavBar.selectHome;
-  late BuildContext context;
-}
